2958f5620e6433d65487798b3650af366b96491a
Dhanush Gopinath <dango2001@gmail.com>
2014-08-28 17:07:07 +0530

    Stringer Interfaces impl added sorting

--- src/StringerInterfaces.go
+++ src/StringerInterfaces.go
@@ -1,6 +1,7 @@
 package main
 
 import "fmt"
+import "sort"
 
 type Organ struct {
 	Name string
@@ -17,10 +18,27 @@ func (g Grams) String() string {
 	return fmt.Sprintf("%dg", int(g))
 }
 
+type Organs []* Organ
+
+func (s Organs) Len() int { return len(s)}
+
+func (s Organs) Swap(i,j int) { s[i], s[j] = s[j],s[i]}
+
+type ByWeight struct {
+	Organs
+}
+
+func (s ByWeight) Less (i,j int) bool {
+	return s.Organs[i].Weight < s.Organs[j].Weight
+}
+
 func main () {
 
 	s:=[]*Organ {{"brain", 340}, {"hand", 89}}
 	for _,organ:= range s{
 		fmt.Println(organ)
 	}
+	sort.Sort(ByWeight {s})
+	fmt.Println(" Sorted " , s)
+
 }
